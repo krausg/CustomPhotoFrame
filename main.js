fb4ee3718c65ae9d8b4b7d78a11a5fbef31413f5
krausg <you@example.com>
2018-05-16 19:19:49 +0200

    using routes

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 'use strict'
 
-var express = require('express');
+var routes = require('./routes');
+var express = require('express');   
 var app = express();
 
 
-app.get('/', function (request, response) {
-    response.write('Hello WORLD');
-    response.end();
-});
+app.get('/', routes.home);
+
+
 app.listen(3000, function (request) {
     console.log("okay, listen little shit");
 });
-/*var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Worl2d!');
-}).listen(8080);    
-*/
 
 
+ 
---
+++ routes/index.js
@@ -0,0 +1,4 @@
+exports.home = function (request, response) {
+    response.write('Hello WORLD');
+    response.end();
+};
