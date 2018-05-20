050d2710c2dfb21b5c12d0fa685804a744c0e4b2
krausg <you@example.com>
2018-05-20 19:15:01 +0200

    component correct extract into smaller components

--- public/vue.js
+++ public/vue.js
@@ -3,61 +3,54 @@ var buttonGrid = Vue.component('buttonGrid', {
      `
 });
 
-
-var grid = Vue.component('grid', {
+var divSplit = Vue.component('div-split', {
   template: `
     <div> 
-      <div :style="styleObject">
-        Hello <button @click="buttonPressed" > {{ currentDepth }}</button>
-        <grid v-if="currentDepth>0" :depth="currentDepth" color="blue"/>
-        <grid v-if="currentDepth>0" :depth="currentDepth" color="green"/>
-      </div>
+        <slot name="left-pane" :class="{isVertical: vertical, left}" />
+        <slot name="right-pane" :class="{isVertical: vertical, right}" />
     </div>
   `,
   props: {
-    depth: {
-      default: 2,
-      type: Number
-    },
-    color: {
-      type: String,
-      default: "crimson"
+    isVertical: {
+      default: true
     }
   },
   data() {
     return {
-      styleObject: {
-        width: '500px',
-        height: '500px',
-        backgroundColor: this.color
+      vertical: {
+        height: "100%",
+        width: "50%",
+        position: "fixed",
+        zIndex: "1",
+        top: "0",
+        overflowX: "hidden",
+        paddingTop: "20px"
+      },
+      left: {
+        left: "0",
+        backgroundColor: "#111"
+      },
+      right: {
+        right: "0",
+        backgroundColor: "red"
       }
-      , currentDepth: this.depth
     }
   },
   methods: {
-    buttonPressed() {
-      this.styleObject.backgroundColor = "blue";
-    }
   },
   components: {
-    grid: grid
+    divSplit: divSplit,
+    right: divSplit
   },
   created() {
-    this.currentDepth--;
-    // var Button = Vue.extend(buttonGrid)
-    // var ButtonInstance = new Button()
-    // ButtonInstance.$mount() // pass nothing
-    // console.log(this.btnGrid)
-    // this.$refs.container.appendChild(btnGrid.$el)
-
     return;
   }
 
 });
 
-
-
-
 new Vue({
-  el: "#app"
+  el: "#app",
+  components: {
+    divSplit: divSplit
+  }
 });  
